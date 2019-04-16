ea185a8220c361f15f81441132f913cc58f53e83
João Moura <joao.moura@b-i.com>
2019-04-17 00:51:55 +0100

    First settings to graphql parser

---
+++ src/api/graphql/typeBuilder.ts
@@ -0,0 +1,71 @@
+import { resolverById } from '@root/api/graphql/resolver'
+import config from '@root/api/utils/configLoader'
+import { ITableInfo } from '@root/configGenerator'
+import Debug from 'debug'
+import {
+  GraphQLBoolean,
+  GraphQLFieldResolver,
+  GraphQLFloat,
+  GraphQLID,
+  GraphQLInputObjectType,
+  GraphQLInt,
+  GraphQLInterfaceType,
+  GraphQLList,
+  GraphQLNonNull,
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLUnionType,
+} from 'graphql'
+
+const debug = Debug('funfunzmc:graphql-type-builder')
+
+const MATCHER: {
+  [key: string]: any
+} = {
+  'varchar(255)': GraphQLString,
+  'int(11)': GraphQLInt,
+  'tinyint(1)': GraphQLBoolean,
+}
+
+const types: {
+  [key: string]: GraphQLObjectType,
+} = {}
+
+export function buildType(table: ITableInfo) {
+  debug(`Creating ${table.name}`)
+  if (!types[table.name]) {
+    types[table.name] = new GraphQLObjectType({
+      name: table.name,
+      description: `${table.name} Type`,
+      fields: () => {
+        const result: {
+          [key: string]: any
+        } = {}
+        table.columns.forEach(
+          (column) => {
+            if (!column.relation && (table.pk === column.name || MATCHER[column.type])) {
+              result[column.name] = {
+                type: table.pk === column.name ? GraphQLID : MATCHER[column.type],
+                description: column.verbose,
+              }
+            }
+
+            if (column.relation) {
+              const relation = column.relation
+              result[column.verbose] = {
+                type: buildType(config().settings.filter(
+                  (settingsTable) => settingsTable.name === relation.table
+                )[0]),
+                description: column.verbose,
+                resolve: resolverById(table, column),
+              }
+            }
+          }
+        )
+        return result
+      },
+    })
+    debug(`Created ${table.name}`)
+  }
+  return types[table.name]
+}
--- test/api.test.ts
+++ test/api.test.ts
@@ -1,5 +1,5 @@
 import request from 'supertest'
-import app from '../lib/api'
+import app from '../src/api'
 import config from './configs/MCconfig'
 import settings from './configs/MCsettings'
 
@@ -230,6 +230,52 @@ describe('routes', () => {
       }).then(
       (response) => {
         return expect(response.status).toBe(401)
+      }
+    )
+  })
+})
+
+describe('graphql', () => {
+  it('graphql endpoint should return status 200', (done) => {
+    return request(application)
+      .post('/graphql')
+      .send({
+        query: `{
+          families {
+            id
+          }
+        }`,
+      })
+      .set('Accept', 'application/json').end(
+      (err, response) => {
+        if (err) {
+          return done(err)
+        }
+        expect(response.status).toBe(200)
+        return done()
+      }
+    )
+  })
+  it('graphql endpoint with deep queries should return 200', (done) => {
+    return request(application)
+      .post('/graphql')
+      .send({
+        query: `{
+          products {
+            id
+            FamilyId {
+              id
+            }
+          }
+        }`,
+      })
+      .set('Accept', 'application/json').end(
+      (err, response) => {
+        if (err) {
+          return done(err)
+        }
+        expect(response.status).toBe(200)
+        return done()
       }
     )
   })
