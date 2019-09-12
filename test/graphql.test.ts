4cb6292f11fc1f11843677fbb79aef5d007eebc2
joaogsleite <joao.leite@isobar.com>
2019-09-12 17:14:55 +0100

    graphql tests

--- test/graphql.test.ts
+++ test/graphql.test.ts
@@ -80,4 +80,83 @@ describe('graphql', () => {
       }
     )
   })
+
+  it('graphql endpoint with many to many relations', (done) => {
+    return request(application)
+      .post('/graphql')
+      .send({
+        query: `{
+          users {
+            id
+            roles {
+              id
+            }
+          }
+        }`,
+      })
+      .set('Accept', 'application/json').end(
+      (err, response) => {
+        if (err) {
+          return done(err)
+        }
+        expect(response.status).toBe(200)
+        expect(response.body).toBeTruthy()
+        const data = response.body.data
+        expect(data.users[0].roles[0]).toBeTruthy()
+        return done()
+      }
+    )
+  })
+  it('graphql endpoint with many to one relations', (done) => {
+    return request(application)
+      .post('/graphql')
+      .send({
+        query: `{
+          images {
+            id
+            products {
+              id
+            }
+          }
+        }`,
+      })
+      .set('Accept', 'application/json').end(
+      (err, response) => {
+        if (err) {
+          return done(err)
+        }
+        expect(response.status).toBe(200)
+        expect(response.body).toBeTruthy()
+        const data = response.body.data
+        expect(data.images[0].products[0]).toBeTruthy()
+        return done()
+      }
+    )
+  })
+  it('graphql endpoint with one to many relations', (done) => {
+    return request(application)
+      .post('/graphql')
+      .send({
+        query: `{
+          products {
+            id
+            images {
+              id
+            }
+          }
+        }`,
+      })
+      .set('Accept', 'application/json').end(
+      (err, response) => {
+        if (err) {
+          return done(err)
+        }
+        expect(response.status).toBe(200)
+        expect(response.body).toBeTruthy()
+        const data = response.body.data
+        expect(data.products[0].images[0]).toBeTruthy()
+        return done()
+      }
+    )
+  })
 })
