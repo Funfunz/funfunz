e9b07ca672045051d7e8dd884a75ba1f1737597d
João Moura <joao.moura@b-i.com>
2019-09-20 14:06:49 +0100

    Added authServer

---
+++ test/utils.ts
@@ -0,0 +1,20 @@
+import express, { Express } from 'express'
+
+export function authenticatedServer(funfunz: Express) {
+  const server = express()
+  server.use((req: any, res, next) => {
+    req.user = {
+      id: 1,
+      email: 'jwebcoder@mymail.com',
+      name: 'João Moura',
+      roles: [
+        { id: 1, name: 'aunthenticated' },
+        { id: 2, name: 'member' },
+        { id: 3, name: 'admin' },
+      ],
+    }
+    next()
+  })
+  server.use(funfunz)
+  return server
+}
