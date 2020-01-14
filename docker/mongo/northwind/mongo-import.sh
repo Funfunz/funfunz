1ebcfb5e05bfca6035005dd46e75de0a7f9a8700
João Moura <joao.moura@b-i.com>
2020-01-14 00:35:22 +0000

    Better user creation for mongo docker

--- docker/mongo/northwind/mongo-import.sh
+++ docker/mongo/northwind/mongo-import.sh
@@ -1,7 +1,8 @@
+echo "hello"
 for f in *.csv
 do
     filename=$(basename "$f")
     extension="${filename##*.}"
     filename="${filename%.*}"
-    mongoimport -d Northwind -c "$filename" --type csv --file "$f" --headerline
+    mongoimport --username=admin --password=password --authenticationDatabase admin -d Northwind -c "$filename" --type csv --file "$f" --headerline
 done
--- docker/mongo/start.sh
+++ docker/mongo/start.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-docker run -v $PWD/docker/mongo/northwind:/db -p 27017:27017 -d --rm --name mongofunfunz -it mongo
-docker exec --workdir /db mongofunfunz bash -c "sleep 1 && ls -la && mongo Northwind --eval \"printjson(db.dropDatabase())\" && chmod +x mongo-import.sh && ./mongo-import.sh"
-docker exec --workdir /db mongofunfunz bash -c "mongo admin --eval \"db.createUser({user: 'admin',pwd: 'password',roles: [{ role: 'userAdminAnyDatabase', db:'admin'}]})\""
-docker restart mongofunfunz
+docker run -v $PWD/docker/mongo/northwind:/db -p 27017:27017 \
+  -e MONGO_INITDB_ROOT_USERNAME=admin \
+  -e MONGO_INITDB_ROOT_PASSWORD=password \
+  -d --rm --name mongofunfunz -it mongo
+docker exec --workdir /db mongofunfunz bash -c "sleep 3 && mongo -u admin -p password --authenticationDatabase admin Northwind --eval 'printjson(db.dropDatabase())' && chmod +x mongo-import.sh && ./mongo-import.sh"
