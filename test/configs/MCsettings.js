e51f2449ce0027792601d519ea7b9f718a8d4227
joaogsleite <joao.leite@isobar.com>
2020-01-15 16:46:56 +0000

    fixed mcsettings for simpler db model

--- test/configs/MCsettings.js
+++ test/configs/MCsettings.js
@@ -1,15 +1,11 @@
 /* istanbul ignore file */
 import usersRolesModel from './models/usersroles'
-import familiesModel from './models/families'
-import imagesModel from './models/images'
 import productsModel from './models/products'
 import rolesModel from './models/roles'
 import usersModel from './models/users'
 
 export default [
   usersRolesModel,
-  familiesModel,
-  imagesModel,
   productsModel,
   rolesModel,
   usersModel,
