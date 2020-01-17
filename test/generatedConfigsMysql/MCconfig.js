9ac1d6ceef610c1df4fdc025c47625715af26cad
joaogsleite <joao.leite@isobar.com>
2020-01-17 15:58:23 +0000

    fixed mcconfig for tests

---
+++ test/generatedConfigsMysql/MCconfig.js
@@ -0,0 +1,12 @@
+export default {
+  "mysql": {
+    "host": "127.0.0.1",
+    "database": "test_db",
+    "user": "root",
+    "password": "",
+    "port": "3306"
+  },
+  "server": {
+    "port": 3004
+  }
+}
