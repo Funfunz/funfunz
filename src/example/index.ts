318b7ec97acd58c96d3f79a369d9bf88eb56e08a
joaogsleite <joao.leite@isobar.com>
2020-11-13 17:43:16 +0000

    deploy

--- src/example/index.ts
+++ src/example/index.ts
@@ -10,14 +10,15 @@ if (process.env.JAWSDB_URL) {
   const DB_URL = process.env.JAWSDB_URL
   const userAndPass = DB_URL.split('@')[0].split('mysql://')[1]
   const [user, password] = userAndPass.split(':')
-  const [host, database] = DB_URL.split('@')[1].split('/')
+  const [hostname, database] = DB_URL.split('@')[1].split('/')
+  const [host, port = '3306'] = hostname.split(':')
   config.connectors.mainDatabase.config = {
     client: 'mysql2',
     host,
     database: database.split('?')[0],
     user,
     password,
-    port: '3306',
+    port
   }
   config.server.port = process.env.PORT || config.server.port
   console.log('mysql config', config.connectors.mainDatabase.config)
