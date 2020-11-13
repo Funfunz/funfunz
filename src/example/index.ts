add3777a556c103ad4818eb463ee08cccf826215
joaogsleite <joao.leite@isobar.com>
2020-11-13 17:47:37 +0000

    deploy on master

--- src/example/index.ts
+++ src/example/index.ts
@@ -3,8 +3,6 @@ import config from '../test/configs/MCconfig'
 import settings from '../test/configs/MCsettings'
 import httpServer from './httpServer'
 
-console.log('JAWSDB_URL', process.env.JAWSDB_URL)
-
 if (process.env.JAWSDB_URL) {
   console.log('JAWSDB_URL', process.env.JAWSDB_URL)
   const DB_URL = process.env.JAWSDB_URL
