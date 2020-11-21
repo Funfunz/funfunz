1b80bec9724adb4d40fb432db10e8262e5249e80
joaogsleite <joao.leite@isobar.com>
2020-11-21 12:17:51 +0000

    FUNFUNZ-51: new configs for demo project

---
+++ src/demo/configs/models/images.ts
@@ -0,0 +1,159 @@
+export default {
+  'name': 'images',
+  'connector': 'mainDatabase',
+  'visible': true,
+  'roles': {
+    'create': [
+      'all'
+    ],
+    'read': [
+      'all'
+    ],
+    'update': [
+      'all'
+    ],
+    'delete': [
+      'all'
+    ]
+  },
+  'properties': [
+    {
+      'name': 'id',
+      'searchable': true,
+      'visible': {
+        'list': true,
+        'detail': true,
+        'relation': true
+      },
+      'model': {
+        'type': 'bigint',
+        'allowNull': false,
+        'isPk': true
+      },
+      'layout': {
+        'label': 'Id',
+        'listColumn': {},
+        'editField': {}
+      }
+    },
+    {
+      'name': 'name',
+      'searchable': true,
+      'visible': {
+        'list': true,
+        'detail': true,
+        'relation': false
+      },
+      'model': {
+        'type': 'varchar(255)',
+        'allowNull': true
+      },
+      'layout': {
+        'label': 'Name',
+        'listColumn': {},
+        'editField': {
+          'type': 'text'
+        }
+      }
+    },
+    {
+      'name': 'main',
+      'searchable': true,
+      'visible': {
+        'list': true,
+        'detail': true,
+        'relation': false
+      },
+      'model': {
+        'type': 'tinyint(1)',
+        'allowNull': false
+      },
+      'layout': {
+        'label': 'Main',
+        'listColumn': {},
+        'editField': {
+          'type': 'checkbox'
+        }
+      }
+    },
+    {
+      'name': 'createdAt',
+      'searchable': true,
+      'visible': {
+        'list': true,
+        'detail': false,
+        'relation': false
+      },
+      'model': {
+        'type': 'datetime',
+        'allowNull': false
+      },
+      'layout': {
+        'label': 'CreatedAt',
+        'listColumn': {},
+        'editField': {
+          'type': 'date'
+        }
+      }
+    },
+    {
+      'name': 'updatedAt',
+      'searchable': true,
+      'visible': {
+        'list': true,
+        'detail': false,
+        'relation': false
+      },
+      'model': {
+        'type': 'datetime',
+        'allowNull': false
+      },
+      'layout': {
+        'label': 'UpdatedAt',
+        'listColumn': {},
+        'editField': {
+          'type': 'date'
+        }
+      }
+    },
+    {
+      'name': 'ProductId',
+      'searchable': true,
+      'visible': {
+        'list': true,
+        'detail': true,
+        'relation': false
+      },
+      'model': {
+        'type': 'int',
+        'allowNull': true
+      },
+      'layout': {
+        'label': 'ProductId',
+        'listColumn': {},
+        'editField': {
+          'type': 'number'
+        }
+      }
+    }
+  ],
+  'layout': {
+    'label': 'Images',
+    'listPage': {},
+    'searchField': {},
+    'createButton': {},
+    'editButton': {},
+    'deleteButton': {},
+    'editPage': {
+      'sections': []
+    }
+  },
+  'relations': [
+    {
+      'type': 'n:1',
+      'relationalTable': 'images',
+      'foreignKey': 'ProductId',
+      'remoteTable': 'products'
+    }
+  ]
+}
