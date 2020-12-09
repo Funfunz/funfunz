9dc7e80004440933eb4546f3ded3abdd8f667658
João Moura <joao87moura@gmail.com>
2020-12-09 19:58:53 +0000

    updated funfunz config types, updated demo app, added the possibility to add queries/mutations to funfunz

--- src/middleware/types.ts
+++ src/middleware/types.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+import { Thunk, GraphQLFieldConfigMap } from 'graphql'
 import { IConfig, ISettings } from '../generator/configurationTypes'
 
 export interface IUser {
@@ -13,9 +14,11 @@ export interface IFunfunzResponse extends express.Response {
   data?: Record<string, unknown>
 }
 
-export interface IFunfunzConfig {
+export interface IFunfunzConfig<QSource = any, QContext = any> {
   config: IConfig
   settings: ISettings
+  queries?: Thunk<GraphQLFieldConfigMap<QSource, QContext>>
+  mutations?: Thunk<GraphQLFieldConfigMap<QSource, QContext>>
 }
 
 export interface IFunfunzRequest extends express.Request {
