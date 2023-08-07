c16c932ad43bef5049195dbbf5d148d3a39d5fba
Joao Moura <joao87moura@gmail.com>
2023-08-07 22:43:32 +0100

    removed unused functions

--- src/middleware/dataConnector/index.ts
+++ src/middleware/dataConnector/index.ts
@@ -15,14 +15,6 @@ export const initDataConnectors = (funfunz: Funfunz): void => {
   )
 }
 
-export const stopDataConnectors = (): void => {
-  Object.entries(connectors).forEach(
-    ([key]) => {
-      connectors[key].stop()
-    }
-  )
-}
-
 export const query = (connectorName: string, args: IQueryArgs): Promise<unknown[] | unknown> => {
   return connectors[connectorName].query(args)
 }
